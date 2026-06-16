5b64501f697a4fd936ab14a63870192e55afe0a4
Tais Duarte Bernardi <tahysduarte22@gmail.com>
2026-06-15 21:05:33 -0300

    fDATABASE_URL

--- src/data/services/prod.service.ts
+++ src/data/services/prod.service.ts
@@ -5,18 +5,13 @@ export class ProdService {
     return {
       type: 'mysql',
 
-      host: process.env.DB_HOST ?? 'localhost',
-      port: parseInt(process.env.DB_PORT ?? '3306', 10),
-      username: process.env.DB_USERNAME ?? 'root',
-      password: process.env.DB_PASSWORD ?? '',
-      database: process.env.DB_DATABASE ?? 'peoplecore',
+      url: process.env.DATABASE_URL,
 
       autoLoadEntities: true,
 
       synchronize: false,
       logging: false,
 
-      // ✔ opções válidas de estabilidade
       retryAttempts: 10,
       retryDelay: 3000,
     };
