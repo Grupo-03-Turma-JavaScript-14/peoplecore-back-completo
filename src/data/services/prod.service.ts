6182f041b9d6f053aec53467a1f17c1267770f9f
Tais Duarte Bernardi <tahysduarte22@gmail.com>
2026-06-15 22:25:26 -0300

    deploy

--- src/data/services/prod.service.ts
+++ src/data/services/prod.service.ts
@@ -2,15 +2,19 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export class ProdService {
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    // ESTA LINHA VAI MOSTRAR NO LOG DO RAILWAY O QUE ESTÁ CHEGANDO
-    console.log('--- TESTE DE URL DO BANCO ---:', process.env.DATABASE_URL);
+
+    console.log('DATABASE_URL =', process.env.DATABASE_URL);
+    console.log('DB_HOST =', process.env.DB_HOST);
+    console.log('DB_PORT =', process.env.DB_PORT);
+    console.log('DB_DATABASE =', process.env.DB_DATABASE);
+    console.log('DB_USERNAME =', process.env.DB_USERNAME);
 
     return {
       type: 'mysql',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: false,
-      logging: false,
+      logging: true,
       retryAttempts: 10,
       retryDelay: 3000,
     };
