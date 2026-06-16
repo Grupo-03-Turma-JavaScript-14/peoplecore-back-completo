4032149a1a60899560d50b31a7595e0d404c7442
Tais Duarte Bernardi <tahysduarte22@gmail.com>
2026-06-16 01:51:16 -0300

    enable synchronize

--- src/data/services/prod.service.ts
+++ src/data/services/prod.service.ts
@@ -2,16 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export class ProdService {
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    console.log('DATABASE_URL:', process.env.DATABASE_URL);
-
     return {
       type: 'mysql',
       url: process.env.DATABASE_URL,
 
       autoLoadEntities: true,
 
-      // TEMPORÁRIO
-      synchronize: true,
+      synchronize: true, // <- mudar aqui
+
       logging: true,
     };
   }
