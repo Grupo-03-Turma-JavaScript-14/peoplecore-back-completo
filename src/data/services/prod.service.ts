324b0eab58690fb1676504e7121ff47340f995f6
Tais Duarte Bernardi <tahysduarte22@gmail.com>
2026-06-16 02:03:56 -0300

    enable

--- src/data/services/prod.service.ts
+++ src/data/services/prod.service.ts
@@ -1,4 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Empresa } from '../../empresa/entities/empresa.entity';
+import { Filial } from '../../empresa/entities/filial.entity';
+import { Contrato } from '../../empresa/entities/contrato.entity';
 
 export class ProdService {
   createTypeOrmOptions(): TypeOrmModuleOptions {
@@ -6,12 +9,14 @@ export class ProdService {
       type: 'mysql',
       url: process.env.DATABASE_URL,
 
-      autoLoadEntities: true,
+      entities: [
+        Empresa,
+        Filial,
+        Contrato,
+      ],
 
       synchronize: true,
-
       logging: true,
-
       logger: 'advanced-console',
     };
   }
