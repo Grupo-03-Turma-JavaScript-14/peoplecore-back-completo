b3da537f0bcab0c337a8cabd81564e6bf96a9c0f
Tais Duarte Bernardi <tahysduarte22@gmail.com>
2026-06-15 22:52:52 -0300

    Deploy

--- src/data/services/prod.service.ts
+++ src/data/services/prod.service.ts
@@ -2,24 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export class ProdService {
   createTypeOrmOptions(): TypeOrmModuleOptions {
-
-    console.log('DB_HOST =', process.env.DB_HOST);
-    console.log('DB_PORT =', process.env.DB_PORT);
-    console.log('DB_DATABASE =', process.env.DB_DATABASE);
-    console.log('DB_USERNAME =', process.env.DB_USERNAME);
+    console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
     return {
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: false,
-      logging: true,
-      retryAttempts: 10,
-      retryDelay: 3000,
+      logging: false,
     };
   }
 }
